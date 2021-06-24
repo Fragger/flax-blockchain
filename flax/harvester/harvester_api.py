Chia-Network/chia-blockchain

"chia farm summary" remote harvester info
Info about plots is obtained from the farmer via
harvesters' farmer_info messages, instead of by
contacting a local harvester.

When multiple harvesters are in use this gives an
estimate of time-to-win based on all plots, not
just local plots. It also makes it easy to
confirm that the harvesters are communicating
with the farmer.

chia/harvester/harvester_api.py
@@ -16,7 +16,7 @@ from chia.server.ws_connection import WSChiaConnection
 from chia.types.blockchain_format.proof_of_space import ProofOfSpace
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.util.api_decorators import api_request, peer_required
-from chia.util.ints import uint8, uint32, uint64
+from chia.util.ints import uint8, uint32, uint64, uint128
 from chia.wallet.derive_keys import master_sk_to_local_sk
 
 
@@ -175,13 +175,15 @@ class HarvesterAPI:
 
         awaitables = []
         passed = 0
-        total = 0
+        total_plots = 0
+        total_space = 0
         for try_plot_filename, try_plot_info in self.harvester.provers.items():
             try:
                 if try_plot_filename.exists():
                     # Passes the plot filter (does not check sp filter yet though, since we have not reached sp)
                     # This is being executed at the beginning of the slot
-                    total += 1
+                    total_plots += 1
+                    total_space += try_plot_info.file_size
                     if ProofOfSpace.passes_plot_filter(
                         self.harvester.constants,
                         try_plot_info.prover.get_id(),
@@ -219,7 +221,8 @@ class HarvesterAPI:
             now,
             uint32(passed),
             uint32(total_proofs_found),
-            uint32(total),
+            uint32(total_plots),
+            uint128(total_space),
         )
         pass_msg = make_msg(ProtocolMessageTypes.farming_info, farming_info)
         await peer.send_message(pass_msg)

chia/protocols/farmer_protocol.py
@@ -6,7 +6,7 @@ from blspy import G2Element
 from chia.types.blockchain_format.pool_target import PoolTarget
 from chia.types.blockchain_format.proof_of_space import ProofOfSpace
 from chia.types.blockchain_format.sized_bytes import bytes32
-from chia.util.ints import uint8, uint32, uint64
+from chia.util.ints import uint8, uint32, uint64, uint128
 from chia.util.streamable import Streamable, streamable
 
 """
@@ -58,6 +58,7 @@ class FarmingInfo(Streamable):
     passed: uint32
     proofs: uint32
     total_plots: uint32
+    total_plot_space: uint128
 
 
 @dataclass(frozen=True)

Fragger/flax-blockchain

flax/harvester/harvester_api.py
@@ -16,7 +16,7 @@ from flax.server.ws_connection import WSFlaxConnection
 from flax.types.blockchain_format.proof_of_space import ProofOfSpace
 from flax.types.blockchain_format.sized_bytes import bytes32
 from flax.util.api_decorators import api_request, peer_required
-from flax.util.ints import uint8, uint32, uint64
+from flax.util.ints import uint8, uint32, uint64, uint128
 from flax.wallet.derive_keys import master_sk_to_local_sk
 
 
@@ -175,13 +175,15 @@ class HarvesterAPI:
 
         awaitables = []
         passed = 0
-        total = 0
+        total_plots = 0
+        total_space = 0
         for try_plot_filename, try_plot_info in self.harvester.provers.items():
             try:
                 if try_plot_filename.exists():
                     # Passes the plot filter (does not check sp filter yet though, since we have not reached sp)
                     # This is being executed at the beginning of the slot
-                    total += 1
+                    total_plots += 1
+                    total_space += try_plot_info.file_size
                     if ProofOfSpace.passes_plot_filter(
                         self.harvester.constants,
                         try_plot_info.prover.get_id(),
@@ -219,7 +221,8 @@ class HarvesterAPI:
             now,
             uint32(passed),
             uint32(total_proofs_found),
-            uint32(total),
+            uint32(total_plots),
+            uint128(total_space),
         )
         pass_msg = make_msg(ProtocolMessageTypes.farming_info, farming_info)
         await peer.send_message(pass_msg)

flax/protocols/farmer_protocol.py
@@ -6,7 +6,7 @@ from blspy import G2Element
 from flax.types.blockchain_format.pool_target import PoolTarget
 from flax.types.blockchain_format.proof_of_space import ProofOfSpace
 from flax.types.blockchain_format.sized_bytes import bytes32
-from flax.util.ints import uint8, uint32, uint64
+from flax.util.ints import uint8, uint32, uint64, uint128
 from flax.util.streamable import Streamable, streamable
 
 """
@@ -58,6 +58,7 @@ class FarmingInfo(Streamable):
     passed: uint32
     proofs: uint32
     total_plots: uint32
+    total_plot_space: uint128
 
 
 @dataclass(frozen=True)